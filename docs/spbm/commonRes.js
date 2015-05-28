9611c3a7cf9e461839c65f395955913b5c089bbc
Michael Rice <michael.rice@rackspace.com>
2015-05-27 19:22:21 -0500

    added spbm docs

---
+++ docs/spbm/commonRes.js
@@ -0,0 +1,2 @@
+var ID_Copyright = "&copy; 2013-2015 VMware, Inc. All rights reserved.";
+var ID_VersionInformation = "Revision 15-Jan-2015 &nbsp;|&nbsp;VMware Storage Policy API &nbsp;|&nbsp; Version 2.0";
